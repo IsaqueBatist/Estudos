0e6a3edbadbe5312106bd2f225f53215ea23593e
Isaque Barbosa <isaquebatista400@gmail.com>
2024-05-19 09:40:15 -0300

    Adicionando o curso da Huawei

--- Python/CEV/Desafios Mundo 3/Desafio103.py
+++ Python/CEV/Desafios Mundo 3/Desafio103.py
@@ -1,10 +1,11 @@
 '''Faça um programa que tenha um função chamada ficha(),
-que receba doi parametros opcionais: o nome, de um jogador
+que receba dois parametros opcionais: o nome, de um jogador
 e quantos gols ele marcou
 O progrma deverá ser capaz de mostrar a ficha do jogador, mesmo
-que lagum dadno não tenha sido informado corretamente'''
+que lagum dado não tenha sido informado corretamente'''
 def ficha(jog='<desconhecido>',gol=0):
-  print(f'O jogador {jog} fez {gol} gol(s) no campeonato')
+  print(f'O jogador {jog} fez {gol} gol
+        (s) no campeonato')
 
 n = str(input("Nome do jogador: "))
 g = str(input("Números de gols "))
